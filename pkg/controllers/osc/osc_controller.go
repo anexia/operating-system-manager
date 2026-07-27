3ae877514a8ad9525339ec18d4d32627592f735d
Kim <kfehrs@anexia-it.com>
2026-07-27 13:40:45 +0200

    apply changes from anexia/1.8.0

--- pkg/controllers/osc/osc_controller.go
+++ pkg/controllers/osc/osc_controller.go
@@ -290,12 +290,6 @@ func (r *Reconciler) reconcileOperatingSystemConfigs(ctx context.Context, md *cl
 		return fmt.Errorf("failed to generate %s osc: %w", oscName, err)
 	}
 
-	if osc.Spec.CloudProvider.Name == "edge" {
-		if err := r.generateEdgeScript(ctx, md, token, bootstrapKubeconfig); err != nil {
-			return fmt.Errorf("failed to generate edge provider bootstrap script: %w", err)
-		}
-	}
-
 	// Add machine deployment revision to OSC
 	revision := md.Annotations[mcsdkcommon.RevisionAnnotation]
 	mdhash, err := r.calculateAnnotationsHash(md.Annotations)
@@ -316,6 +310,13 @@ func (r *Reconciler) reconcileOperatingSystemConfigs(ctx context.Context, md *cl
 		return fmt.Errorf("failed to create %s osc: %w", oscName, err)
 	}
 	r.log.Infof("successfully generated provisioning osc: %v", oscName)
+
+	if osc.Spec.CloudProvider.Name == "edge" {
+		if err := r.generateEdgeScript(ctx, md, token, bootstrapKubeconfig); err != nil {
+			return fmt.Errorf("failed to generate edge provider bootstrap script: %w", err)
+		}
+	}
+
 	return nil
 }
 
@@ -522,8 +523,44 @@ func (r *Reconciler) generateEdgeScript(ctx context.Context, md *clusterv1alpha1
 
 	serverURL := config.Clusters[clusterName].Server
 	bootstrapSecretName := fmt.Sprintf("%s-%s-bootstrap-config", md.Name, md.Namespace)
-	script := fmt.Sprintf("apt-get update -y\napt-get install jq -y\ncurl -s -k -v --header 'Authorization: Bearer %s' %s/api/v1/namespaces/cloud-init-settings/secrets/%s | jq '.data[\"cloud-config\"]' -r| base64 -d > /etc/cloud/cloud.cfg.d/%s.cfg \ncloud-init --file /etc/cloud/cloud.cfg.d/%s.cfg init\nsystemctl enable bootstrap.service\nsystemctl restart bootstrap.service\n",
-		token, serverURL, bootstrapSecretName, bootstrapSecretName, bootstrapSecretName)
+
+	getConfig, err := providerconfig.GetConfig(md.Spec.Template.Spec.ProviderSpec)
+	if err != nil {
+		return err
+	}
+	var script string
+	if getConfig.OperatingSystem == "flatcar" {
+		script = fmt.Sprintf(`
+curl -s -k -v --header 'Authorization: Bearer %s' %s/api/v1/namespaces/cloud-init-settings/secrets/%s \
+  | jq '.data["cloud-config"]' -r \
+  | base64 -d > /tmp/%s.cfg
+
+coreos-cloudinit --from-file /tmp/%s.cfg 
+
+`, token, serverURL, bootstrapSecretName, bootstrapSecretName, bootstrapSecretName)
+	}
+	if getConfig.OperatingSystem == "ubuntu" {
+		script = fmt.Sprintf(`
+apt-get update -y
+apt-get install jq -y
+curl -s -k -v --header 'Authorization: Bearer %s' %s/api/v1/namespaces/cloud-init-settings/secrets/%s \
+  | jq '.data["cloud-config"]' -r \
+  | base64 -d > /etc/cloud/cloud.cfg.d/%s.cfg
+
+
+# Compare the semver values of cloud-init versions to determine the correct command to run.
+# This is required because the command line arguments for cloud-init changed in version 24.1, for details: https://github.com/canonical/cloud-init/releases/tag/24.1.
+export CLOUD_INIT_VERSION=$(cloud-init --version | awk '{print $2}')
+if [[ $(echo -e "24.0.0\n$CLOUD_INIT_VERSION" | sort -V | head -n1) = "24.0.0" ]]; then
+	cloud-init init --file /etc/cloud/cloud.cfg.d/%s.cfg  
+else
+  cloud-init --file /etc/cloud/cloud.cfg.d/%s.cfg init
+fi
+
+systemctl enable bootstrap.service
+systemctl restart bootstrap.service
+`, token, serverURL, bootstrapSecretName, bootstrapSecretName, bootstrapSecretName, bootstrapSecretName)
+	}
 
 	scriptSecretName := fmt.Sprintf("edge-provider-script-%s-%s", md.Name, md.Namespace)
 	secret := &corev1.Secret{}
@@ -541,7 +578,11 @@ func (r *Reconciler) generateEdgeScript(ctx context.Context, md *clusterv1alpha1
 	secret.Namespace = bootstrap.CloudInitNamespace
 	secret.Data["fetch-bootstrap-script"] = []byte(script)
 
-	return r.workerClient.Create(ctx, secret)
+	err = r.workerClient.Create(ctx, secret)
+	if err != nil {
+		return fmt.Errorf("failed to create %s secret in namespace %s: %w", secret.Name, bootstrap.CloudInitNamespace, err)
+	}
+	return nil
 }
 
 // filterMachineDeploymentPredicate will filter machine deployments based on the presence of OSP annotation
